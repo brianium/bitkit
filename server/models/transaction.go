c4442e521f1ffdf417624665de3350356c855ced
Brian Scaturro <scaturrob@gmail.com>
2018-03-08 10:15:18 -0500

    uses naked return for InsertTransactions and defers commit rollback

--- server/models/transaction.go
+++ server/models/transaction.go
@@ -12,39 +12,37 @@ type Transaction struct {
 }
 
 // InsertTransactions does a batch insert of all the given transactions
-func (db *DB) InsertTransactions(transactions []*Transaction) error {
+func (db *DB) InsertTransactions(transactions []*Transaction) (err error) {
 	txn, err := db.Begin()
 	if err != nil {
-		return err
+		return
 	}
+
+	defer func() {
+		if err != nil {
+			txn.Rollback()
+			return
+		}
+		err = txn.Commit()
+	}()
 
 	stmt, err := txn.Prepare(pq.CopyInSchema("bitkit", "transactions", "id", "fee_rate", "weight"))
 	if err != nil {
-		return err
+		return
 	}
 
 	for _, transaction := range transactions {
 		_, err = stmt.Exec(transaction.ID, transaction.FeeRate, transaction.Weight)
 		if err != nil {
-			return err
+			return
 		}
 	}
 
 	_, err = stmt.Exec()
 	if err != nil {
-		return err
+		return
 	}
 
 	err = stmt.Close()
-	if err != nil {
-		return err
-	}
-
-	err = txn.Commit()
-	if err != nil {
-		txn.Rollback()
-		return err
-	}
-
-	return nil
+	return
 }
