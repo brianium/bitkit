b253fdce330ba119eeff3fa4354fff04459516d8
Brian Scaturro <scaturrob@gmail.com>
2018-04-15 09:30:37 -0400

    Updates view per ryan's feedback

--- client/bitkit/src/cljs/bitkit/subs.cljs
+++ client/bitkit/src/cljs/bitkit/subs.cljs
@@ -1,17 +1,23 @@
 (ns bitkit.subs
   (:require [re-frame.core :as re-frame]))
 
+(def blocksize-in-bytes 4194304)
+
 (re-frame/reg-sub
  ::transaction-id
  (fn [db]
    (or (:transaction-id db) "")))
 
 (re-frame/reg-sub
-  ::transaction
-  (fn [db]
-    (:transaction db)))
-
-(re-frame/reg-sub
   ::error
   (fn [db]
     (:error db)))
+
+(re-frame/reg-sub
+  ::transaction
+  (fn [{:keys [transaction]}]
+    (when transaction
+      (let [{:keys [fee_rate, weight, total_weight]} transaction]
+        (-> transaction
+            (assoc :fee (* fee_rate weight))
+            (assoc :capacity_used (/ total_weight blocksize-in-bytes)))))))
--- client/bitkit/src/cljs/bitkit/views.cljs
+++ client/bitkit/src/cljs/bitkit/views.cljs
@@ -15,31 +15,40 @@
 
 (defn transaction-form
   [{:keys [txid]}]
-  [:form {:on-submit (handler #(set-path! (str "/" txid)))}
+  [:form.content {:on-submit (handler #(set-path! (str "/" txid)))}
    [:div.field
     [:label.label "Transaction ID"]
     [:div.control
      [:input.input
       {:value     txid
-       :on-change #(re-frame/dispatch [::events/set-transaction-id (.. % -target -value)])}]]
-    [:p.help "Bitcoin transaction id"]]])
+       :on-change #(re-frame/dispatch [::events/set-transaction-id (.. % -target -value)])}]]]])
 
 (defn notification
   [{:keys [error]}]
   (when error
-    [:div.notification.is-warning
-     "The given transaction ID could not be found in the mempool. This means
-     it has already been confirmed or never existed in the first place. We should
-     totally work on the wording/experience of this message"]))
+    [:div.notification.is-warning.content
+     [:p
+      "The given transaction ID could not be found in the mempool.
+      This can happen for a variety of reasons:"]
+     [:ul
+      [:li "The transaction ID was entered incorrectly"]
+      [:li "The transaction has already been confirmed"]
+      [:li "The transaction has been evicted"]]]))
 
 (defn transaction
   [{:keys [txn]}]
   (when txn
-    [:div.has-text-centered
-     [:p
-      [:span.is-size-3 (:transaction_count txn)]
-      " transactions ahead of you"]
-     [:p (str "totaling " (:total_weight txn) " whatever units ryan tell me")]]))
+    [:section
+     [:div.content.is-small
+      [:h2 "Your transaction"]
+      [:ul.is-marginless.is-unstyled
+       [:li (str "Fee: " (:fee txn) " satoshis")]
+       [:li (str "Fee rate: " (:fee_rate txn) " satoshis per byte")]]]
+     [:div.content.is-small
+      [:h2 "Transactions with a higher fee rate"]
+      [:ul.is-marginless.is-unstyled
+       [:li (str "Count: " (:transaction_count txn))]
+       [:li (str "Block capacity used: " (:capacity_used txn))]]]]))
 
 (defn main-panel []
   (let [txid  (re-frame/subscribe [::subs/transaction-id])
