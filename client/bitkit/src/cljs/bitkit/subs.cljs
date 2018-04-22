535ad22f0ef07dcdf8dc124315696cd6af1daa53
Brian Scaturro <scaturrob@gmail.com>
2018-04-22 13:21:42 -0400

    Improves numeric formatting

--- client/bitkit/src/cljs/bitkit/subs.cljs
+++ client/bitkit/src/cljs/bitkit/subs.cljs
@@ -1,5 +1,7 @@
 (ns bitkit.subs
-  (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame]
+            [goog.string :as gstring]
+            [goog.string.format]))
 
 (def blocksize-in-bytes 1048576)
 
@@ -19,5 +21,9 @@
     (when transaction
       (let [{:keys [fee_rate, weight, total_weight]} transaction]
         (-> transaction
-            (assoc :fee (* fee_rate weight))
-            (assoc :capacity_used (/ total_weight blocksize-in-bytes)))))))
+            (assoc :fee (Math/round (* fee_rate weight)))
+            (assoc :fee_rate (gstring/format "%.1f" fee_rate))
+            (assoc :capacity_used
+              (-> (/ total_weight blocksize-in-bytes)
+                  (as-> capacity (gstring/format "%.1f" capacity))
+                  (str "%"))))))))
